0123ef70717dafd6c434d7afd558dfc5137d05ff
ichim-david <ichim.david@gmail.com>
2014-11-10 16:09:37 +0200

    * Feature: added rdfexport of progressLevel if eea.rdfmarshaller is installed [ichim-david refs #21617]

---
+++ eea/progressbar/rdf/marshaller.py
@@ -0,0 +1,47 @@
+""" RDF Marshaller module for progressbar """
+import surf
+from zope.component import adapts, queryAdapter
+from zope.interface import implements
+from eea.progressbar.interfaces import IBaseObject, IWorkflowProgress
+from eea.rdfmarshaller.interfaces import ISurfResourceModifier
+
+
+class ProgressLevelSurfModifier(object):
+    """ Progress Level workflow surf information
+    """
+
+    implements(ISurfResourceModifier)
+    adapts(IBaseObject)
+
+    def __init__(self, context):
+        self.context = context
+        surf.ns.register(wf="http://intelleo.eu/ontologies/workflow/ns#")
+
+    def run(self, resource, *args, **kwds):
+        """ Adds the wf progress level rdf resource
+            <wf:currentStatus>
+              <wf:DetailedProgressStatus  rdf:about="workflow_progress_level">
+                <wf:progressLevel>30</wf:progressLevel>
+              </wf:DetailedProgressStatus>
+            </wf:currentStatus>
+        """
+        context = self.context
+        output = []
+        progress = queryAdapter(context, IWorkflowProgress)
+        if not progress:
+            return output
+        session = resource.session
+        store = session.get_default_store()
+
+        progress_level = progress.progress
+        wf_surf = surf.ns.WF
+        store.reader.graph.bind('wf', wf_surf, override=True)
+        details_progress = session.get_class(surf.ns.WF.DetailedProgressStatus)
+        rdfp = session.get_resource("#workflow_state_progress",
+                                    details_progress)
+        rdfp[surf.ns.WF['progressLevel']] = progress_level
+        rdfp.update()
+
+        output.append(rdfp)
+        resource['wf_currentStatus'] = output
+        resource.save()
