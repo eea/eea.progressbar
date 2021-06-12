3062f122e86d57d7957e5971453dd6af693d4bae
David Ichim <ichim.david@gmail.com>
2021-06-12 22:23:53 +0300

    Refs #131062 add field info to storage when adding schema

--- eea/progressbar/content/events.py
+++ eea/progressbar/content/events.py
@@ -2,6 +2,8 @@
 """
 from logging import getLogger
 logger = getLogger('eea.progressbar')
+from zope.component import  queryAdapter
+from eea.progressbar.interfaces import IStorage
 
 def add_schema(obj, event):
     """ Add schema object
@@ -14,6 +16,11 @@ def add_schema(obj, event):
         oid = '.schema'
         if oid not in obj.objectIds():
             oid = obj.invokeFactory(ctype, id=oid)
+            res_obj = obj[oid]
+            fields = res_obj.schema.fields()
+            storage = queryAdapter(obj, IStorage)
+            for field in fields:
+                storage.add_field(field.getName())
         obj[oid].unindexObject()
     except Exception, err:
         logger.exception(err)
